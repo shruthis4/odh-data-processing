0cd584bc20518c52377ef6e19d7af7bc483bf284
shruthi sankepelly <41649151+shruthis4@users.noreply.github.com>
2025-12-17 17:04:08 -0500

    Remove empty new line

--- kubeflow-pipelines/common/constants.py
+++ kubeflow-pipelines/common/constants.py
@@ -1,3 +1,3 @@
 # Base container images used across all Docling Kubeflow Pipelines
 PYTHON_BASE_IMAGE = "quay.io/aipcc/docling/cuda-ubi9"
-DOCLING_BASE_IMAGE = "quay.io/aipcc/docling/cuda-ubi9" 
+DOCLING_BASE_IMAGE = "quay.io/aipcc/docling/cuda-ubi9"
