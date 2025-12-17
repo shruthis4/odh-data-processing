15fb7534fd1d9ece323d06c2f641575180a69b22
shruthi sankepelly <41649151+shruthis4@users.noreply.github.com>
2025-12-17 09:09:41 -0500

    Update base container images for Docling Pipelines

--- kubeflow-pipelines/common/constants.py
+++ kubeflow-pipelines/common/constants.py
@@ -1,3 +1,3 @@
 # Base container images used across all Docling Kubeflow Pipelines
-PYTHON_BASE_IMAGE = "registry.access.redhat.com/ubi9/python-311:9.6-1755074620"
-DOCLING_BASE_IMAGE = "quay.io/fabianofranz/docling-ubi9:2.54.0"
+PYTHON_BASE_IMAGE = "quay.io/repository/amaredia/aipcc-docling-image"
+DOCLING_BASE_IMAGE = "quay.io/repository/amaredia/aipcc-docling-image"
